acfcee1cd6e827981c2cf54e56ce22224396a795
suneel kumar yarramuthala <yarramuthalasunil@gmail.com>
2022-11-26 13:59:08 +0530

    fully completed the logic of the game

--- js/main.js
+++ js/main.js
@@ -9,6 +9,11 @@ var eight = document.querySelector("#eight");
 var nine = document.querySelector("#nine");
 let start = document.querySelector("#start");
 let reset = document.querySelector("#reset");
+let message = document.querySelector(".message");
+let ones = document.querySelector("#oneS");
+let twos = document.querySelector("#twoS");
+let scoreone = 0;
+let scoretwo = 0;
 let count = 0;
 let oneimg = 0;
 let twoimg = 0;
@@ -31,7 +36,8 @@ let br;
 
 console.log(document.querySelector("#oneimg").getAttribute("src"));
 
-start.addEventListener("click", () => {
+start.addEventListener("click", resetGame);
+function resetGame() {
   document.querySelector("#oneimg").setAttribute("src", "");
   document.querySelector("#twoimg").setAttribute("src", "");
   document.querySelector("#threeimg").setAttribute("src", "");
@@ -41,13 +47,40 @@ start.addEventListener("click", () => {
   document.querySelector("#sevenimg").setAttribute("src", "");
   document.querySelector("#eightimg").setAttribute("src", "");
   document.querySelector("#nineimg").setAttribute("src", "");
+  message.innerHTML = "Player One's turn to paly";
+  count = 0;
+  oneimg = 0;
+  twoimg = 0;
+  threeimg = 0;
+  fourimg = 0;
+  fiveimg = 0;
+  siximg = 0;
+  sevenimg = 0;
+  eightimg = 0;
+  nineimg = 0;
+  tl = undefined;
+  tm = undefined;
+  tr = undefined;
+  ml = undefined;
+  mm = undefined;
+  mr = undefined;
+  bl = undefined;
+  bm = undefined;
+  br = undefined;
+}
+reset.addEventListener("click", () => {
+  scoreone = 0;
+  scoretwo = 0;
+  ones.innerHTML = scoreone;
+  twos.innerHTML = scoretwo;
 });
-reset.addEventListener("click", () => {});
 
 one.addEventListener("click", () => {
   if (oneimg == 0) {
     if (count % 2 == 0) {
       document.querySelector("#oneimg").setAttribute("src", "img/cross.png");
+      console.log(count);
+      console.log(count % 2);
       count++;
       tl = 1;
     } else if (count % 2 !== 0) {
@@ -57,6 +90,12 @@ one.addEventListener("click", () => {
     }
     oneimg = 1;
   }
+  if (count % 2 == 0) {
+    message.innerHTML = "Player One's turn to paly";
+  } else {
+    message.innerHTML = "Player Two's turn to paly";
+  }
+  setTimeout(p2, 100);
 });
 two.addEventListener("click", () => {
   if (twoimg == 0) {
@@ -71,6 +110,12 @@ two.addEventListener("click", () => {
     }
     twoimg = 1;
   }
+  if (count % 2 == 0) {
+    message.innerHTML = "Player One's turn to paly";
+  } else {
+    message.innerHTML = "Player Two's turn to paly";
+  }
+  setTimeout(p2, 100);
 });
 three.addEventListener("click", () => {
   if (threeimg == 0) {
@@ -85,6 +130,12 @@ three.addEventListener("click", () => {
     }
     threeimg = 1;
   }
+  if (count % 2 == 0) {
+    message.innerHTML = "Player One's turn to paly";
+  } else {
+    message.innerHTML = "Player Two's turn to paly";
+  }
+  setTimeout(p2, 100);
 });
 four.addEventListener("click", () => {
   if (fourimg == 0) {
@@ -99,6 +150,12 @@ four.addEventListener("click", () => {
     }
     fourimg = 1;
   }
+  if (count % 2 == 0) {
+    message.innerHTML = "Player One's turn to paly";
+  } else {
+    message.innerHTML = "Player Two's turn to paly";
+  }
+  setTimeout(p2, 100);
 });
 five.addEventListener("click", () => {
   if (fiveimg == 0) {
@@ -113,6 +170,12 @@ five.addEventListener("click", () => {
     }
     fiveimg = 1;
   }
+  if (count % 2 == 0) {
+    message.innerHTML = "Player One's turn to paly";
+  } else {
+    message.innerHTML = "Player Two's turn to paly";
+  }
+  setTimeout(p2, 100);
 });
 six.addEventListener("click", () => {
   if (siximg == 0) {
@@ -127,6 +190,12 @@ six.addEventListener("click", () => {
     }
     siximg = 1;
   }
+  if (count % 2 == 0) {
+    message.innerHTML = "Player One's turn to paly";
+  } else {
+    message.innerHTML = "Player Two's turn to paly";
+  }
+  setTimeout(p2, 100);
 });
 seven.addEventListener("click", () => {
   if (sevenimg == 0) {
@@ -141,6 +210,12 @@ seven.addEventListener("click", () => {
     }
     sevenimg = 1;
   }
+  if (count % 2 == 0) {
+    message.innerHTML = "Player One's turn to paly";
+  } else {
+    message.innerHTML = "Player Two's turn to paly";
+  }
+  setTimeout(p2, 100);
 });
 eight.addEventListener("click", () => {
   if (eightimg == 0) {
@@ -155,6 +230,12 @@ eight.addEventListener("click", () => {
     }
     eightimg = 1;
   }
+  if (count % 2 == 0) {
+    message.innerHTML = "Player One's turn to paly";
+  } else {
+    message.innerHTML = "Player Two's turn to paly";
+  }
+  setTimeout(p2, 100);
 });
 nine.addEventListener("click", () => {
   if (nineimg == 0) {
@@ -169,5 +250,94 @@ nine.addEventListener("click", () => {
     }
     nineimg = 1;
   }
+  if (count % 2 == 0) {
+    message.innerHTML = "Player One's turn to paly";
+  } else {
+    message.innerHTML = "Player Two's turn to paly";
+  }
+  setTimeout(p2, 100);
 });
-console.log(eightimg);
+function p2() {
+  if (tl == 0 && tm == 0 && tr == 0) {
+    alert("player two won!");
+    scoretwo += 1;
+    setTimeout(resetGame, 1000);
+  }
+  if (ml == 0 && mm == 0 && mr == 0) {
+    alert("player two won!");
+    scoretwo += 1;
+    setTimeout(resetGame, 1000);
+  }
+  if (bl == 0 && bm == 0 && br == 0) {
+    alert("player two won!");
+    scoretwo += 1;
+    setTimeout(resetGame, 1000);
+  }
+  if (tl == 0 && ml == 0 && bl == 0) {
+    alert("player two won!");
+    scoretwo += 1;
+    setTimeout(resetGame, 1000);
+  }
+  if (tm == 0 && mm == 0 && bm == 0) {
+    alert("player two won!");
+    scoretwo += 1;
+    setTimeout(resetGame, 1000);
+  }
+  if (tr == 0 && mr == 0 && br == 0) {
+    alert("player two won!");
+    scoretwo += 1;
+    setTimeout(resetGame, 1000);
+  }
+  if (tl == 0 && mm == 0 && br == 0) {
+    alert("player two won!");
+    scoretwo += 1;
+    setTimeout(resetGame, 1000);
+  }
+  if (tr == 0 && mm == 0 && bl == 0) {
+    alert("player two won!");
+    scoretwo += 1;
+    setTimeout(resetGame, 1000);
+  }
+  if (tl == 1 && tm == 1 && tr == 1) {
+    alert("player one won!");
+    scoreone += 1;
+    setTimeout(resetGame, 1000);
+  }
+  if (ml == 1 && mm == 1 && mr == 1) {
+    alert("player one won!");
+    scoreone += 1;
+    setTimeout(resetGame, 1000);
+  }
+  if (bl == 1 && bm == 1 && br == 1) {
+    alert("player one won!");
+    scoreone += 1;
+    setTimeout(resetGame, 1000);
+  }
+  if (tl == 1 && ml == 1 && bl == 1) {
+    alert("player one won!");
+    scoreone += 1;
+    setTimeout(resetGame, 1000);
+  }
+  if (tm == 1 && mm == 1 && bm == 1) {
+    alert("player one won!");
+    scoreone += 1;
+    setTimeout(resetGame, 1000);
+  }
+  if (tr == 1 && mr == 1 && br == 1) {
+    alert("player one won!");
+    scoreone += 1;
+    setTimeout(resetGame, 1000);
+  }
+  if (tl == 1 && mm == 1 && br == 1) {
+    alert("player one won!");
+    scoreone += 1;
+    setTimeout(resetGame, 1000);
+  }
+  if (tr == 1 && mm == 1 && bl == 1) {
+    alert("player one won!");
+    scoreone += 1;
+    setTimeout(resetGame, 1000);
+  }
+  ones.innerHTML = scoreone;
+  twos.innerHTML = scoretwo;
+}
